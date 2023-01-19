4ecd1aa655bc3084b677e5ba7a294220d8071a1c
ukpark <14ukpark@hni-gl.com>
2023-01-20 00:32:16 +0900

    http servlet request header info

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,37 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+        printHeaderLine(request);
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("request.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+        System.out.println("request = " + request.getRequestURI());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+    }
+
+    private void printHeaderLine(HttpServletRequest request) {
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+    }
+}
