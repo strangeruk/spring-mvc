d5aea74a385b5c2cb963f1e020d628397d71b051
ukpark <14ukpark@hni-gl.com>
2023-01-27 18:42:00 +0900

    spring mvc - 시작하기

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -9,7 +9,9 @@ import java.util.List;
 import java.util.Map;
 
 public class MemberListControllerV3 implements ControllerV3 {
+
     private final MemberRepository memberRepository = MemberRepository.getInstance();
+
     @Override
     public ModelView process(Map<String, String> paramMap) {
         List<Member> members = memberRepository.findAll();
